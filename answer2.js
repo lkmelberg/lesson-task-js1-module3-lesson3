2356a745421cc4f99ea6e8d7f7ebed79e9ab502e
MJ Phillip <32121892+MJPhillip@users.noreply.github.com>
2021-09-22 13:54:04 +0100

    Update answer2.js

--- answer2.js
+++ answer2.js
@@ -1,4 +1,4 @@
-fetch("https://cat-fact.herokuapp.com/facts")
+fetch("https://noroff.herokuapp.com/v1/cat-facts/facts")
     .then(function (response) {
         return response.json();
     })
@@ -12,7 +12,7 @@ fetch("https://cat-fact.herokuapp.com/facts")
 // we can also enclose the code in a function and then call it
 // this will have the same effect but means the code is reusable and can be called again from somewhere else in the code
 function getCatFacts() {
-    fetch("https://cat-fact.herokuapp.com/facts")
+    fetch("https://noroff.herokuapp.com/v1/cat-facts/facts")
         .then(function (response) {
             return response.json();
         })
@@ -28,7 +28,7 @@ getCatFacts();
 
 // with arrow functions
 function getCatFacts2() {
-    fetch("https://cat-fact.herokuapp.com/facts")
+    fetch("https://noroff.herokuapp.com/v1/cat-facts/facts")
         .then((response) => response.json())
         .then((results) => {
             console.log(results.length);
