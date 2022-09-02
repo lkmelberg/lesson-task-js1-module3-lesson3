ac98146f51fa41de24c34fd937a879c432d32267
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2022-09-02 11:26:41 +0200

    yes

--- js/script.js
+++ js/script.js
@@ -1,42 +1,105 @@
+// // ### Question 1
+
+// // Write code that checks if the below variable is truthy, log the string "truthy" if it is, and "falsy" if not.
+
+// const menuIsOpen = false;
+
+// if (menuIsOpen) {
+//   console.log("truthy");
+// } else {
+//   console.log("falsy");
+// }
+
+// // ### Question 2
+
+// // Based on the below URL, what are the different querystring parameters and their respective values?
+
+// // ```
+// // https://api.rawg.io/api/games?platforms=4&genres=action&publishers=354
+// // ```
+
+// // platforms is a parameter with a value of 4
+// // genres is a parameter with a value of action
+// // publishers is a parameter with a value of 354
+
+// // ### Question 3
+
+// // - Create an async function called `getCatFacts`.
+// // - Inside the function make an API call to the below URL using async/await, and save it to a variable called `response`.
+// // - Create a variable called `results` where you await the json of your response variable.
+// // - Console.log the length of `results`.
+
+// // ```
+// // https://noroff.herokuapp.com/v1/cat-facts/facts
+
+// async function getCatFacts() {
+//   const url = "https://noroff.herokuapp.com/v1/cat-facts/facts";
+//   const response = await fetch(url);
+//   const results = await response.json();
+
+//   console.log(results.length);
+// }
+// getCatFacts();
+
 // ### Question 1
 
-// Write code that checks if the below variable is truthy, log the string "truthy" if it is, and "falsy" if not.
+// Convert the below functions to one-line arrow functions.
 
-const menuIsOpen = false;
+// function greet(name) {
+//   return `Hello ${name}!`;
+// }
 
-if (menuIsOpen) {
-  console.log("truthy");
-} else {
-  console.log("falsy");
-}
+const greet = (name) => `Hello ${name}!`;
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+const add = (a, b) => a + b;
 
 // ### Question 2
 
-// Based on the below URL, what are the different querystring parameters and their respective values?
+// Convert the below code to use the then/catch syntax instead.
 
-// ```
-// https://api.rawg.io/api/games?platforms=4&genres=action&publishers=354
-// ```
+const url = "https://noroff.herokuapp.com/v1/cat-facts/facts";
 
-// platforms is a parameter with a value of 4
-// genres is a parameter with a value of action
-// publishers is a parameter with a value of 354
+// fetch(url)
+//   .then(function (response) {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data.length);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
 
-// ### Question 3
+// or
 
-// - Create an async function called `getCatFacts`.
-// - Inside the function make an API call to the below URL using async/await, and save it to a variable called `response`.
-// - Create a variable called `results` where you await the json of your response variable.
-// - Console.log the length of `results`.
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => console.log(data.length))
+  .catch((error) => console.log(error));
 
-// ```
-// https://noroff.herokuapp.com/v1/cat-facts/facts
+// async function getCatFacts() {
+//   try {
+//     const response = await fetch(
+//       "https://noroff.herokuapp.com/v1/cat-facts/facts"
+//     );
+//     const results = await response.json();
+//     console.log(results.length);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-async function getCatFacts() {
-  const url = "https://noroff.herokuapp.com/v1/cat-facts/facts";
-  const response = await fetch(url);
-  const results = await response.json();
+// enclosed in a function
+function gettCat {
 
-  console.log(results.length);
+
+fetch("https://noroff.herokuapp.com/v1/cat-facts/facts")
+  .then((response) => response.json())
+  .then((data) => console.log(data.length))
+  .catch((error) => console.log(error));
 }
-getCatFacts();
